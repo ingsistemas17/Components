32146c54f61bb0977c8fa24f0dbe7631224deb44
ingsistemas17 <tecsistemas17@gmail.com>
2018-02-24 15:54:12 -0500

    se agrega el componente base DAO para conexiones Sql server y Oracle

---
+++ ComponentBaseDAO/BaseDAO/DAO/OracleBaseDAO.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OracleClient;
+using System.Configuration;
+using System.Data;
+
+namespace DAO
+{
+    public class OracleBaseDAO
+    {
+
+        /// <summary>
+        /// cadena de conexion a la base datos.
+        /// </summary>
+        private string conexion;
+
+
+
+        /// <summary>
+        /// permite la conexion a la base de datos.
+        /// </summary>
+        private OracleConnection con;
+
+        protected OracleBaseDAO()
+        {
+            conexion = ConfigurationManager.AppSettings["ConexionOracle"].ToString();
+
+            con = new OracleConnection();
+        }
+
+
+        /// <summary>
+        /// realiza consultas en la base de datos a partir  de un query
+        /// </summary>
+        /// <param name="parametros"></param>
+        /// <param name="query"></param>
+        /// <returns>Retorna los datos generados por el query</returns>
+        protected DataTable Consultar(OracleParameter [] parametros, String query )
+        {
+            // TODO: Modify the connection string and include any
+            // additional required properties for your database.
+            con.ConnectionString = conexion;
+            OracleCommand cmd = new OracleCommand();
+            DataSet set = new DataSet();
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+                
+
+                if (parametros != null)
+                {
+                    foreach (var parametro in parametros )
+                    {
+                        cmd.Parameters.Add(parametro);
+                    }
+                }
+                //cmd.ExecuteNonQuery();
+                OracleDataAdapter adapter = new OracleDataAdapter(cmd);
+                adapter.Fill(set);
+                return set.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// realiza consultas en la base de datos a partir  de un sp
+        /// </summary>
+        /// <param name="parametros"></param>
+        /// <param name="nameSP"></param>
+        /// <returns>Retorna los datos generados por el query</returns>
+        protected DataTable ConsultarSP(OracleParameter[] parametros, String nameSP)
+        {
+            // TODO: Modify the connection string and include any
+            // additional required properties for your database.
+            con.ConnectionString = conexion;
+            OracleCommand cmd = new OracleCommand();
+            DataSet set = new DataSet();
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+
+                cmd.CommandText = nameSP;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                if (parametros != null)
+                {
+                    foreach (var parametro in parametros)
+                    {
+                        cmd.Parameters.Add(parametro);
+                    }
+                }
+
+
+                cmd.Parameters.Add("pcursor", OracleType.Cursor).Direction = ParameterDirection.Output;
+                OracleDataAdapter adapter = new OracleDataAdapter(cmd);
+                adapter.Fill(set);
+     
+                
+                return set.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// Ejecuta un query en una bd de oracle
+        /// </summary>
+        /// <param name="parametros"></param>
+        /// <param name="query"></param>
+        protected void ejecutar(OracleParameter[] parametros, String query)
+        {
+            con.ConnectionString = conexion;
+            OracleCommand cmd = new OracleCommand();
+            DataSet set = new DataSet();
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+
+                if (parametros != null)
+                {
+                    foreach (var parametro in parametros)
+                    {
+                        cmd.Parameters.Add(parametro);
+                    }
+                }
+                int res = cmd.ExecuteNonQuery();
+
+                if (res <= 0)
+                {
+                    throw new Exception("No se insertaron los datos.");
+                }
+
+            }
+             catch(Exception ex)
+            {
+                 throw new Exception(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ ComponentBaseDAO/BaseDAO/DAO/SQLServerBaseDAO.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DAO
+{
+    /// <summary>
+    /// Clase que permite manejar todo lo referente de acceso a datos para el motor SqlServer.
+    /// </summary>
+   public class SQLServerBaseDAO
+    {
+                
+        /// <summary>
+        /// Cadena de conexion
+        /// </summary>
+        private string Conexion;
+        /// <summary>
+        /// Permite la conexion  a la base datos
+        /// </summary>
+        private static SqlConnection Cone;
+
+
+
+        /// <summary>
+        /// establece la conexion al motor de base datos con una conexión por defecto "conexionSqlServer" del .config 
+        /// </summary>
+        public SQLServerBaseDAO ()
+        {            //Cadena de Conexion
+            Conexion = ConfigurationManager.ConnectionStrings["conexionSqlServer"].ConnectionString;
+            Cone = new SqlConnection(Conexion);
+        }
+
+        /// <summary>
+        /// establece la conexion al motor de base datos 
+        /// </summary>
+        /// <param name="cadenaConexion">recibe el nombre de la cadena de conexión del .config</param>
+        public SQLServerBaseDAO(string cadenaConexion)
+        {            //Cadena de Conexion
+            Conexion = ConfigurationManager.ConnectionStrings[cadenaConexion].ConnectionString;
+            Cone = new SqlConnection(Conexion);
+        }
+
+
+        /// <summary>
+        /// abre la conexion Sql
+        /// </summary>
+        public void abrirConexion()
+        {
+            Cone.Open();
+        }
+    
+        /// <summary>
+        /// cierra la conexion sql
+        /// </summary>
+        public void CerrarConexion()
+        {
+            Cone.Close();
+        }
+
+
+
+        /// <summary>
+        /// permite realizar consultas de información a traves de procedures recibe una lista de parametros.
+        /// </summary>
+        /// <param name="nombreSP"></param>
+        /// <param name="parametros"></param>
+        /// <returns>retorna DataTable como resultado del procedure </returns>
+        protected DataTable Consultar(string nombreSP, List<SqlParameter> parametros)
+        {
+            DataTable DtCompraul = new DataTable();
+            try
+            {
+                SqlCommand SqlComando = new SqlCommand(nombreSP, Cone);
+                SqlDataAdapter SqlDA = new SqlDataAdapter();
+
+                SqlComando.Parameters.Clear();
+                SqlComando.CommandType = CommandType.StoredProcedure;
+
+                foreach (var IDP in parametros)
+                {
+                    SqlComando.Parameters.Add(IDP);
+                }
+                
+                SqlDA.SelectCommand = SqlComando;
+
+                SqlDA.Fill(DtCompraul);
+                
+                return DtCompraul;
+            }
+
+            catch (Exception e)
+            {
+                 throw new Exception(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// permite realizar consultas de información a traves de stored procedures recibe un array de parametros.
+        /// </summary>
+        /// <param name="nombreSP"></param>
+        /// <param name="parametros"></param>
+        /// <returns>retorna DataTable como resultado del SP </returns>
+        public DataTable Consultar(string nombreSP, SqlParameter [] parametros)
+        {
+            DataTable DtCompraul = new DataTable();
+            try
+            {
+                SqlCommand SqlComando = new SqlCommand(nombreSP, Cone);
+                SqlDataAdapter SqlDA = new SqlDataAdapter();
+
+                SqlComando.Parameters.Clear();
+                SqlComando.CommandType = CommandType.StoredProcedure;
+
+                if(parametros != null)
+                foreach (var IDP in parametros)
+                {
+                    SqlComando.Parameters.Add(IDP);
+                }
+
+                SqlDA.SelectCommand = SqlComando;
+
+                SqlDA.Fill(DtCompraul);
+
+                return DtCompraul;
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+
+        /// <summary>
+        /// permite realizar la ejecucion de información a traves de procedures util para insertar, eliminar, actulizar  recibe una lista de parametros.
+        /// </summary>
+        /// <param name="nombreSP"></param>
+        /// <param name="parametros"></param>
+        protected void ejecutar(string nombreSP, List<SqlParameter> parametros)
+        {
+            DataTable DtCompraul = new DataTable();
+            try
+            {
+                SqlCommand SqlComando = new SqlCommand(nombreSP, Cone);
+                SqlDataAdapter SqlDA = new SqlDataAdapter();
+
+                SqlComando.Parameters.Clear();
+                SqlComando.CommandType = CommandType.StoredProcedure;
+
+                foreach (var IDP in parametros)
+                {
+                    SqlComando.Parameters.Add(IDP);
+                }
+
+                SqlComando.ExecuteNonQuery();
+
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// permite realizar la ejecucion de información a traves de procedures util para insertar, eliminar, actulizar  recibe un array de parametros.
+        /// </summary>
+        /// <param name="nombreSP"></param>
+        /// <param name="parametros"></param>
+        public void ejecutar (string nombreSP, SqlParameter [] parametros )
+        {
+            DataTable DtCompraul = new DataTable();
+            try
+            {
+                SqlCommand SqlComando = new SqlCommand(nombreSP, Cone);
+                SqlDataAdapter SqlDA = new SqlDataAdapter();
+
+                SqlComando.Parameters.Clear();
+                SqlComando.CommandType = CommandType.StoredProcedure;
+
+                foreach (var IDP in parametros)
+                {
+                    SqlComando.Parameters.Add(IDP);
+                }
+
+                SqlComando.ExecuteNonQuery();
+
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
